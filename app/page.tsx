6cc972923de06390afedbc28947782a9eb9d781b
MohammadMaasir <maasir554@gmail.com>
2025-03-17 21:08:24 +0530

    hero gradient

--- app/page.tsx
+++ app/page.tsx
@@ -15,22 +15,25 @@ export default function Home() {
       
 
       {/* Hero Section */}
-      <section className="relative h-screen flex items-center">
-        <div className="absolute inset-0 z-0">
+      <section className="relative h-screen flex items-center bg-gradient-to-b from-yellow-500/10 to-indigo-500/10 md:from-transparent">
+        <div className="absolute z-0 w-full">
           <Image
             src="/team/hero.jpg"
             alt="E-Cell Background"
-            fill
-            className={cn("[mask-image:linear-gradient(270deg,transparent_0%,white_10%,white_90%,transparent_100%),linear-gradient(0deg,transparent_0%,rgb(220,220,220)_30%,rgb(220,220,220)_35%,rgba(10,10,10,0.2)_80%,transparent_100%),radial-gradient(circle,white_75%,transparent_90%)]","[mask-composite:intersect]","object-cover brightness-50")}
+            // fill
+            width={1920}
+            height={1080}
+            className={cn("[mask-image:linear-gradient(270deg,transparent_0%,white_10%,white_90%,transparent_100%),linear-gradient(0deg,transparent_0%,rgb(220,220,220)_30%,rgb(220,220,220)_35%,rgba(10,10,10,0.2)_60%,transparent_100%),radial-gradient(circle,white_75%,transparent_90%)]","[mask-composite:intersect]","object-cover brightness-50")}
             priority
           />
         </div>
-        <div className={"container mx-auto px-4 relative z-10"}>
-          <div className="max-w-2xl space-y-6">
+        <div className={"container mx-auto px-6 relative z-10"}>
+          <div className="max-w-2xl space-y-6 -translate-y-1/2">
             <h1 className="font-montserrat+ text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white">
-              Join the <span className="text-primary">Entrepreneurship Cell</span>
+              Join the<br/> 
+              <span className="text-primary">Entrepreneurship Cell</span>
             </h1>
-            <p className="font-roboto text-lg text-white/90 max-w-xl">
+              <p className="font-inter text-sm sm:text-base text-white font-light max-w-xl">
               Be a part of the most vibrant community at NIT Bhopal. We nurture innovation, entrepreneurship, and
               leadership skills.
             </p>
