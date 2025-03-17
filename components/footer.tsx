f9db0e362358ced2f707b6fd3970080138205818
MohammadMaasir <maasir554@gmail.com>
2025-03-17 21:19:02 +0530

    footer fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -74,7 +74,7 @@ export default function Footer() {
           </div>
 
           {/* Quick Links */}
-          <div>
+          <div className="w-full">
             <h3 className=" text-lg font-bold text-white mb-2">Quick Links</h3>
             <ul className="space-y-2 text-sm">
               <li>
